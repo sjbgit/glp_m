e921a4705b21d2ba01d9c15c1dde49d3e74bcb8f
sjbgit <sjb_strat@hotmail.com>
2015-07-14 18:15:48 -0500

    base

---
+++ gulp.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sbunke on 7/14/2015.
+ */
+module.exports = function() {
+    var config = {
+
+        //all js to vet
+        alljs: [
+            './src/**/*.js',
+            './*.js',
+        ]
+    };
+
+    return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,24 @@
  * Created by sbunke on 7/14/2015.
  */
 var gulp = require('gulp');
-
 var args = require('yargs').argv;
+var config = require('./gulp.config')();
+
+console.log(config);
+
+var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
     log('running vet - analyzer with jshint and jscs');
-    return gulp.src([
-        './src/**/*.js',
-        './*.js',
-    ])
-        .pipe(gulpif(args.verbose, gulpprint()))//if pass in verbose flag to gulp command, then will log all files
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe(jshint.reporter('fail'));
+    //console.log(config);
+    return gulp.src(
+        config.alljs
+    )
+        .pipe($.if(args.verbose, $.print()))//if pass in verbose flag to gulp command, then will log all files
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
 function log(msg) {
